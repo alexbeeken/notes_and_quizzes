07b584d110bf721e293b5956b9b664a27503ce1a
Alex Beeken <aeb242@gmail.com>
2018-01-08 21:08:38 -0800

    add questions for ruby

--- questions.rb
+++ questions.rb
@@ -1,1 +1,26 @@
-IO.puts 'Interview Practice!'
+require 'yaml'
+
+TOPICS = ['ruby', 'rails']
+
+puts 'Interview Practice!'
+puts 'choose topic'
+TOPICS.each_with_index do |topic, index|
+  puts "#{index + 1} - #{topic}"
+end
+topic = TOPICS[gets.to_i - 1]
+
+questions = YAML.load_file("#{topic}.yaml").shuffle
+
+correct = 0
+questions.each do |question|
+  puts question['q']
+  answer = gets.gsub!("\n", '')
+  if answer == question['a'].to_s
+    correct += 1
+    puts 'correct'
+  else
+    puts "incorrect: #{question['a']}"
+  end
+end
+
+puts "You got #{correct} correct out of #{questions.length}"
